4c459a1b96714011e49bbb6fa63face80b3cfcef
MikeBlakeway-Insight <mike.blakeway@insight.com>
2022-10-19 11:52:49 +0100

    Rendering all data for singleRoom

--- src/pages/SingleRoom.jsx
+++ src/pages/SingleRoom.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import {useParams} from 'react-router-dom'
-import {Image} from '../layout'
 import hotels from '../data/accommodation.json'
 import availability from '../data/accommodation_availability.json'
 
@@ -9,14 +8,52 @@ export const SingleRoom = () => {
 	const hotelById = hotels.accommodations.find(hotels => hotels.id === +id)
 	const thisRoom = hotelById.rooms.find(room => room.id === +roomId)
 	const roomAvailability = availability.rooms.find(room => room.id === +roomId)
-	console.log(thisRoom)
+	console.log(roomAvailability)
 
 	return (
 		<section className='px-12'>
+			<h2 className='text-orange-600 text-3xl text-center font-bold px-16'>
+				{thisRoom.name}
+			</h2>
 			<div className='container mx-auto grid grid-cols-2 gap-4 place-items-center'>
-				<h2 className='text-orange-600 text-3xl font-bold mt-4'>
-					<p>{thisRoom.name}</p>
-				</h2>
+				<div className='pt-4 py-6'>
+					<p className='text-orange-600 text-lg font-bold mx-2'>
+						Price:{' '}
+						<span className='inline-block bg-orange-100 rounded-full px-3 py-1 text-xs font-semibold text-orange-500 mr-2 mb-2'>
+							{thisRoom.price.price}
+						</span>
+					</p>
+					<p className='text-orange-600 text-lg font-bold mx-2'>
+						Type:{' '}
+						<span className='inline-block bg-orange-100 rounded-full px-3 py-1 text-xs font-semibold text-orange-500 mr-2 mb-2'>
+							{thisRoom.type}
+						</span>
+					</p>
+					<p className='text-orange-600 text-lg font-bold mx-2'>
+						Max Occupancy:{' '}
+						<span className='inline-block bg-orange-100 rounded-full px-3 py-1 text-xs font-semibold text-orange-500 mr-2 mb-2'>
+							{thisRoom.max_occupancy}
+						</span>
+					</p>
+					<p className='text-orange-600 text-lg font-bold mx-2'>
+						Min Occupancy:{' '}
+						<span className='inline-block bg-orange-100 rounded-full px-3 py-1 text-xs font-semibold text-orange-500 mr-2 mb-2'>
+							{thisRoom.min_occupancy}
+						</span>
+					</p>
+					<p className='text-orange-600 text-lg font-bold mx-2'>
+						Available:{' '}
+						<span className='inline-block bg-orange-100 rounded-full px-3 py-1 text-xs font-semibold text-orange-500 mr-2 mb-2'>
+							{roomAvailability.available}
+						</span>
+					</p>
+					<p className='text-orange-600 text-lg font-bold mx-2'>
+						Total:{' '}
+						<span className='inline-block bg-orange-100 rounded-full px-3 py-1 text-xs font-semibold text-orange-500 mr-2 mb-2'>
+							{roomAvailability.total}
+						</span>
+					</p>
+				</div>
 
 				<div className='pt-4 py-6'>
 					<p className='text-orange-600 text-2xl font-bold mb-4'>Facilities</p>
